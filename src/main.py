0aef1bca507d5d07744540508b67e7815792c61b
Andrea <andrea@kaist.ac.kr>
2022-10-21 07:09:33 +0900

    Updated text

--- src/main.py
+++ src/main.py
@@ -3,6 +3,15 @@ from machine import ADC, Pin
 import time, random
 
 
+# GLobals
+# These are the pins you will use for making the hardware addition
+aPin = Pin(15, Pin.OUT)
+bPin = Pin(14, Pin.OUT)
+cPin = Pin(13, Pin.OUT)
+sumPin = Pin(12, Pin.IN)
+carryoutPin = Pin(11, Pin.IN)
+
+
 # Helpers
 
 # Get a random integer
@@ -18,6 +27,7 @@ def displayRow(row, number):
 # bitSum takes two bits (b1 and b2) and add them
 # the function returns a tuple with the result (0 or 1)
 # and the carryover (0 or 1)
+# the addition is done in hardware (write a/b/c pins and read sumPin/carryoutPin)
 def bitSum(b1, b2, carry):
     result = 0
     carry = 0
@@ -29,6 +39,7 @@ def bitSum(b1, b2, carry):
 # sumRow takes two numbers (n1 and n2) which are 5 bits longs
 # and sum them together. The results is a 5-bits long number
 # the carryover (0 or 1) shows whether the addition overflowed
+# sumRow calls bitSum because the addition is performed in hardware
 def sumRow(n1, n2):
     result = 0b10101  # a placeholder value in binary to change
     carry = 1  # a placeholder value in binary to change
